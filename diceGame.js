59ecf2915c099bfd544bca07f20a106578f5c5da
josephmkotvis <josephmkotvis@gmail.com>
2017-11-01 14:15:53 -0500

    Made dice rolls into functions

--- diceGame.js
+++ diceGame.js
@@ -1,10 +1,20 @@
 "Use Strictly"
 function runGame (){
-let d4 = Math.floor(Math.random() * 4) + 1
-let d6 = (Math.floor(Math.random() * 6) + 1) * .95
-let d8 = (Math.floor(Math.random() * 8) + 1) * .90
-let d12 = (Math.floor(Math.random() * 12) + 1) * .85
-let d20 = (Math.floor(Math.random() * 20) + 1) * .825
-console.log(d20);
+function rollLvl1(){
+	let d4 = Math.floor(Math.random() * 4) + 1
 }
+function rollLvl2(){
+	let d6 = (Math.floor(Math.random() * 6) + 1) * .95
+}
+function rollLvl3(){
+	let d8 = (Math.floor(Math.random() * 8) + 1) * .90
+}
+function rollLvl4(){
+	let d12 = (Math.floor(Math.random() * 12) + 1) * .85
+}
+function rollLvl5(){
+	let d20 = (Math.floor(Math.random() * 20) + 1) * .825
+}
+
+
 runGame();
