dfbbe619f39b2b8ef55b4bad6a85a1f9663cd97c
josephmkotvis <josephmkotvis@gmail.com>
2017-11-02 08:33:54 -0500

    Made the dice functions without loops

--- diceGame.js
+++ diceGame.js
@@ -1,51 +1,84 @@
 "Use Strictly"
 function runGame (){
-	// let d4 = rollLvl1();
+	let d4 = rollLvl1();
 	let d6 = rollLvl2();
 	let d8 = rollLvl3();
 	let d12 = rollLvl4();
 	let d20 = rollLvl5();
 	// let win1v1Roll = rollLvl1vs1(d4,);
-// function rollLvl1(){ 
-// 	switch(d4){
-// 				case '2,3':
-// 					((Math.floor(Math.random() * 4) + 1)/2)
-// 					break;
-// 				case '1':
-// 					((Math.floor(Math.random() * 4) + 1)*2)
-// 					break;
-// 				default:
-// 					(Math.floor(Math.random() * 4) + 1)
-// 	return d4
-// }
+function rollLvl1(){ 
+	let d4 = (Math.floor(Math.random() * 4) + 1); {
+				if (d4 === 2 || d4 === 3){
+					(d4/=2);
+				}
+				else if (d4 === 1){
+					(d4*=2);
+				}
+				else{
+					(d4 ===d4)
+				}
+			return d4
+			}
+	}
 function rollLvl2(){
-	let d6 = (Math.floor(Math.random() * 6) + 1)
-			for (i=d6; i!==i; i++)
-				if (i === 2,3,5){
-					((i*.95)/2)
+	let d6 = (Math.floor(Math.random() * 6) + 1); {
 				}
-				else if (i === 1){
-					((i*.95)*2)
+				if (d6 === 2 || d6 === 3 || d6 === 5){
+					(d6*=(.95/2));
 				}
-				else if (i === 4,6){
-					(i*.95)
+				else if (d6 === 1){
+					(d6*=(.95*2));
 				}
+				else{
+					(d6 *=.95);
+				}
+				
 			return d6
-}
+			}
 function rollLvl3(){
-	let d8 = (Math.floor(Math.random() * 8) + 1) * .90
-	return d8
-}
+	let d8 = (Math.floor(Math.random() * 8) + 1); {
+				if (d8 === 2 || d8 === 3 || d8 === 5){
+					(d8*=(.90/2));
+				}
+				else if (d8 === 1){
+					(d8=(.90*2));
+				}
+				else {
+					(d8 *=.90);
+				}
+			return d8
+			}
+	}
 function rollLvl4(){
-	let d12 = (Math.floor(Math.random() * 12) + 1) * .85
-	return d12
-}
+	let d12 = (Math.floor(Math.random() * 12) + 1);{
+				if (d12 === 2 || d12 === 3 || d12 === 5 || d12 === 11){
+					(d12*=(.85/2));
+				}
+				else if (d12 === 1 || d12 === 10){
+					(d12*=(.85*2));
+				}
+				else {
+					(d12 *=.85);
+				}
+				
+			return d12
+			}
+	}
 function rollLvl5(){
-	let d20 = (Math.floor(Math.random() * 20) + 1) * .825
-	return d20
-}
-	
-}
+	let d20 = (Math.floor(Math.random() * 20) + 1);{
+				if (d20 === 2 || d20 === 3 || d20 === 5 || d20 === 11 || d20 === 13 || d20 === 17){
+					(d20*=(.80/2));
+				}
+				else if (d20 === 1 || d20 === 10 || d20 === 13){
+					(d20*=(.80*2));
+				}
+				else {
+					(d20 *=.80);
+				}
+				
+			return d20
+			}
+	}	
 function rollLvl1vs1(yourlvl1Roll, opLvl1Roll){
 	let win1v1Roll=true
 		if (yourLvl1Roll > opLvl1Roll){
@@ -55,6 +88,7 @@ function rollLvl1vs1(yourlvl1Roll, opLvl1Roll){
 
 		}
 } 	
+}
 // function rollLvl1vs2(){
 
 // }
